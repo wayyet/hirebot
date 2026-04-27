0ca87f52a4f3b693b61d7fd63e92c0cc685e93d3
jeff <chati_software@hotmail.com>
2026-04-27 14:05:28 +0800

    [upd] 修改apphost;

--- HireBot.AppHost/Program.cs
+++ HireBot.AppHost/Program.cs
@@ -1,11 +1,11 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var postgres = builder.AddPostgres("Postgres");
+var postgres = builder.AddPostgres("postgres");
 
-var db = postgres.AddDatabase("HireBot");
+var hirebot = postgres.AddDatabase("hirebot");
 
-var apiService = builder.AddProject<Projects.HireBot_ApiService>("api")
-    .WithReference(db);
+var apiService = builder.AddProject<Projects.HireBot_ApiService>("hirebot-api")
+    .WithReference(hirebot);
 
 var app = builder.Build();
     
